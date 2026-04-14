9efeeb91e214c03a9433a9441471533db9763565
Mithilesh Singh <146987967+PratapOP@users.noreply.github.com>
2026-04-14 19:51:56 +0530

    Initial Streamlit app

---
+++ .history/student-score-analyzer/utils_20260414193624.py
@@ -0,0 +1,14 @@
+def calculate_average(scores):
+    return sum(scores) / len(scores)
+
+def get_grade(avg):
+    if avg >= 90:
+        return "A+"
+    elif avg >= 80:
+        return "A"
+    elif avg >= 70:
+        return "B"
+    elif avg >= 60:
+        return "C"
+    else:
+        return "D"
